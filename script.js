c00caa20fba2173d53eac51b851d7558c6ccdb23
Abdullah <abdullahayinla15@gmail.com>
2025-05-12 09:49:08 +0100

    van

---
+++ script.js
@@ -0,0 +1,14 @@
+let my_name = "Abdullah"
+let my_age = 17;
+let my_height = 55;
+let my_weight = 40;
+let my_eyes = "Black";
+let my_teeth = "white";
+let my_hair = "Black";
+
+console.log("Let talk about", my_name);
+console.log("He's" , my_height , "inches tall.");
+console.log("He's" , my_weight , "pounds heavy.");
+console.log("Actually that not too heavy.");
+console.log("He's got" , my_eyes , "eyes and" , my_hair , "hair");
+console.log("His teeth are usually" , my_teeth , "depending on the coffee");
